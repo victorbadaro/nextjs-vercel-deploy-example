ec5630edb422edccade027d127169246848f019f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-09-02 18:09:18 -0300

    chore: change home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
+import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Edited file:&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
